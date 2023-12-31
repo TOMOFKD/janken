59a2db7778803bf51b7fc9860355d8035c819d69
TOMOFKD <t86596305@gmail.com>
2024-01-01 03:11:03 +0900

    Submission jankenapp

---
+++ janken.rb
@@ -0,0 +1,120 @@
+hand = ["グー","チョキ","パー"]
+finger = ["上","下","左","右"]
+
+number = true
+janken = true
+aiko = true
+
+while janken
+  puts "じゃんけん．．．"
+  puts "0(グー)1(チョキ)2(パー)3(戦わない)"
+
+  while number
+    player = gets.chomp
+    if player == "0" || player == "1" || player == "2" || player == "3"
+      number = false
+    else
+      puts "0〜3の数字を入力してください"
+      number = true
+    end
+  end
+
+  number = true
+    
+  if player == "3"
+    break
+  end
+  
+  com = rand(3)
+
+  puts "ホイ！"
+  puts "--------------"
+  puts "あなた：#{hand[player.to_i]}を出しました"
+  puts "相手：#{hand[com]}を出しました"
+
+  if player.to_i == com
+    while aiko
+      puts "--------------"
+      puts "あいこで．．．"
+      puts "0(グー)1(チョキ)2(パー)3(戦わない)"
+      while number
+        player = gets.chomp.to_i
+        if player == 0 || player == 1 || player == 2 || player == 3
+          number = false
+        else
+          puts "0〜3の数字を入力してください"
+          number = true
+        end
+      end
+      number = true
+      if player == 3
+        break
+      end
+      com = rand(3)
+      puts "ホイ！"
+      puts "--------------"
+      puts "あなた：#{hand[player.to_i]}を出しました"
+      puts "相手：#{hand[com]}を出しました"
+      if player != com
+        aiko = false
+      end
+    end
+  end
+
+  if player  == 3
+    break
+  end
+
+  if(player == "0" && com == 1)||(player == "1" && com == 2)||(player == "2" && com == 0)
+    puts "--------------"
+    puts "あっち向いて〜"
+    puts "0(上)1(下)2(左)3(右)"
+    while number
+      player = gets.chomp.to_i
+      if player == 0 || player == 1 || player == 2 || player == 3
+        number = false
+      else
+        puts "0〜3の数字を入力してください"
+        number = true
+      end
+    end
+    number = true
+    com = rand(4)
+    puts "ホイ！"
+    puts "--------------"
+    puts "あなた：#{finger[player]}を出しました"
+    puts "相手：#{finger[com]}を出しました"
+    if player == com
+      puts "あなたの勝ちです"
+      janken = false
+      #return janken
+    end
+
+  else
+    puts "--------------"
+    puts "あっち向いて〜"
+    puts "0(上)1(下)2(左)3(右)"
+    while number
+      player = gets.chomp.to_i
+      if player == 0 || player == 1 || player == 2 || player == 3
+        number = false
+      else
+        puts "0〜3の数字を入力してください"
+        number = true
+      end
+    end
+    number = true
+    com = rand(4)
+    puts "ホイ！"
+    puts "--------------"
+    puts "あなた：#{finger[player]}を出しました"
+    puts "相手：#{finger[com]}を出しました"
+    if player == com
+      puts "あなたの負けです"
+      janken = false
+      #return janken
+    end
+  end
+  
+  aiko = true
+end
